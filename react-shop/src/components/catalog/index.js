f92be173ddbe4a97704a11751eded6c3eb55920f
joshuamontevirgen <joshua2936@gmail.com>
2022-04-03 20:08:35 +0800

    react animation

--- react-shop/src/components/catalog/index.js
+++ react-shop/src/components/catalog/index.js
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Item as CatalogItem } from "./item";
 import { API_URL } from "../../constants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList, faGrip as faGrid } from "@fortawesome/free-solid-svg-icons";
 import { Filter } from "./filter";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./../../styles.css";
 
 //https://medium.com/@joncalumdiprose_40937/filtering-multiple-queries-with-react-useeffect-9b0de068876d
 //https://stackoverflow.com/questions/54069253/usestate-set-method-not-reflecting-change-immediately
+//https://typeofnan.dev/how-to-animate-items-out-of-an-array-in-react/
+
+//findDomMode is deprecated warning - https://github.com/reactjs/react-transition-group/issues/668
+
 export const Index = () => {
   const [isLoading, setLoading] = useState(true);
   const [filteredList, setFilteredList] = useState([]);
   const [filters, setFilters] = useState([]);
   const [items, setItems] = useState([]);
   const [isGrid, setGrid] = useState(true);
+  const nodeRef = useRef(null);
 
   //get items
   useEffect(() => {
@@ -29,51 +36,68 @@ export const Index = () => {
 
   //filter
   useEffect(() => {
+    const filterList = () => {
+      if (!filters.length) return items;
+      let filtered = items.filter((item) => {
+        var include = false;
+        filters.forEach((filter) => {
+          if (item.category === filter.value) include = true;
+        });
+        return include;
+      });
+      return filtered;
+    };
     setFilteredList(filterList());
   }, [filters]);
-
-  function filterList() {
-    if (!filters.length) return items;
-    let filtered = items.filter((item) => {
-      var include = false;
-      filters.forEach((filter) => {
-        if (item.category === filter.value) include = true;
-      });
-      return include;
-    });
-    return filtered;
-  }
 
   const handleFilterChange = (selected) => {
     setFilters([...selected]);
   };
 
   return (
-    <div>
-      <div>
-        <Filter onChange={handleFilterChange} />
-      </div>
-      <button className="btn " onClick={() => setGrid(!isGrid)}>
-        <FontAwesomeIcon icon={isGrid ? faList : faGrid} />
-      </button>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="row col-12 m-0 p-1">
-          <ul className="d-flex flex-wrap" style={{ listStyleType: "none" }}>
-            {filteredList.map((item, index) => {
-              return (
-                <li
-                  key={item.id}
-                  className={"m-1 p-1 " + (!isGrid && "col-12")}
-                >
-                  <CatalogItem item={item} isGrid={isGrid} />
-                </li>
-              );
-            })}
-          </ul>
+    <>
+      <div className="container-fluid bg-primary">
+        <div className="row pt-2 pb-2 bg-secondary">
+          <div className="col-5">
+            <Filter onChange={handleFilterChange} />
+          </div>
+          <button className="btn " onClick={() => setGrid(!isGrid)}>
+            <FontAwesomeIcon icon={isGrid ? faList : faGrid} />
+          </button>
         </div>
-      )}
-    </div>
+      </div>
+      <div className="container-fluid">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="row col-12 m-0 p-1">
+            <TransitionGroup
+              component="ul"
+              className="d-flex flex-wrap"
+              style={{ listStyleType: "none" }}
+            >
+              {filteredList.map((item, index) => {
+                return (
+                  <CSSTransition
+                    // nodeRef={nodeRef}
+                    key={item.id}
+                    timeout={250}
+                    classNames="item"
+                  >
+                    <li className={"m-1 p-1 " + (!isGrid && "col-12")}>
+                      <CatalogItem
+                        item={item}
+                        //  ref={nodeRef}
+                        isGrid={isGrid}
+                      />
+                    </li>
+                  </CSSTransition>
+                );
+              })}
+            </TransitionGroup>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
--- react-shop/src/components/catalog/item.js
+++ react-shop/src/components/catalog/item.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 //https://getbootstrap.com/docs/5.0/components/card/
 
-export const Item = ({ item, isGrid }) => {
-  return (
-    <div className="card">
-      <div className={isGrid ? "" : "row g-0"}>
-        <div className={isGrid ? "" : "col-md-4"}>
-          <img className="card-img-top" src={item.imageUrl}></img>
-        </div>
-        <div className={isGrid ? "" : "col-md-8"}>
-          <div className="card-body">
-            <h3 className="card-title">{item.name}</h3>
-            <p className="card-text">{item.desc}</p>
-            <p className="card-text">P{item.price}</p>
-          </div>
+export const Item = React.forwardRef(({ item, isGrid }, ref) => (
+  <div className="card" ref={ref}>
+    <div className={isGrid ? "" : "row "}>
+      <div className={isGrid ? "" : "col-md-4"}>
+        <img className="" src={item.imageUrl}></img>
+      </div>
+      <div className={isGrid ? "" : "col-md-8"}>
+        <div className="card-body">
+          <h3 className="card-title">{item.name}</h3>
+          <p className="card-text">{item.desc}</p>
+          <p className="card-text">P{item.price}</p>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+));
