b60041878b54f30f1075e930b592e890ab180f4d
joshuamontevirgen <joshua2936@gmail.com>
2022-04-14 23:51:31 +0800

    url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const COOKIE_JWT_TOKEN_NAME = "token";
 export const COOKIE_EMAIL_NAME = "email";
-export const API_URL = "https://localhost:44329";
+export const API_URL = "https://app-joshuastore-eastasia-dev.azurewebsites.net";
