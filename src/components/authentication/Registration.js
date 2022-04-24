9812b314e2cba6994ae1aca0d71629b4a89dca01
joshuamontevirgen <joshua2936@gmail.com>
2022-04-24 19:39:48 +0800

    login, registration form

--- src/components/authentication/Registration.js
+++ src/components/authentication/Registration.js
@@ -1,47 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setCookie } from "../../helpers/cookies";
-import { COOKIE_JWT_TOKEN_NAME, API_URL } from "../../constants";
-import {
-  setAuthenticated,
-  setUsername,
-  setJwtToken,
-} from "./authenticationSlice";
-
-//todo -  refresh, access token
-//https://stackoverflow.com/questions/27067251/where-to-store-jwt-in-browser-how-to-protect-against-csrf?rq=1
-//https://dev.to/cotter/localstorage-vs-cookies-all-you-need-to-know-about-storing-jwt-tokens-securely-in-the-front-end-15id
+import { API_URL } from "../../constants";
+import { useForm } from "../utility/useForm";
 
 export default function Registration() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [confirmPasswordError, setConfirmPasswordError] = useState("");
-  const dispatch = useDispatch();
-
-  function validateForm() {
-    var isValid = true;
-    if (!(email.length > 0 && password.length > 0)) {
-      isValid = false;
-    }
-
-    return isValid;
-  }
+  const [isValid, setValid] = useState(false);
+  const [formData, submitting, handleSubmit, handleChange] = useForm();
 
   useEffect(() => {
-    if (password && confirmPassword) {
-      password !== confirmPassword
-        ? setConfirmPasswordError("Password does not match")
-        : setConfirmPasswordError("");
+    var valid = true;
+    if (formData.password !== formData.confirmPassword) {
+      setConfirmPasswordError("Password does not match");
+      valid = false;
+    } else {
+      setConfirmPasswordError("");
+      valid = valid && true;
     }
-  }, [password, confirmPassword]);
+    setValid(valid);
+  }, [formData]);
 
   const handleRegister = async (e) => {
     e.preventDefault();
     if (!confirmPasswordError) {
-      const response = await registerUser({ Email: email, Password: password });
+      const response = await registerUser({
+        Email: formData.email,
+        Password: formData.password,
+      });
       alert(response.message);
       if (response.success) {
         navigate("/login");
@@ -61,77 +48,79 @@ export default function Registration() {
   }
 
   return (
-    <div className="items-stretch justify-center flex item-center ">
-      <div className="w-full max-w-xs ">
-        <form
-          className="bg-white shadow1 rounded px-8 pt-6 pb-8 mt-20"
-          onSubmit={handleRegister}
-        >
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              className=" font-light shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              placeholder="Email"
-              required
-            />
-          </div>
+    <>
+      <div className="items-stretch justify-center flex item-center ">
+        <div className="w-full max-w-xs ">
+          <form
+            className="bg-white shadow1 rounded px-8 pt-6 pb-8 mt-20"
+            onSubmit={(e) => {
+              e.preventDefault();
+              isValid && handleSubmit(e, handleRegister);
+            }}
+          >
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                className=" font-light shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={handleChange}
+                type="email"
+                placeholder="Email"
+                name="email"
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              className={`shadow appearance-none font-light border rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline ${
-                confirmPasswordError && "border-red-700"
-              }`}
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="******************"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700  mb-2"
-              htmlFor="ConfirmPassword"
-            >
-              Confirm Password
-            </label>
-            <input
-              className={`shadow appearance-none font-light border rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline ${
-                confirmPasswordError && "border-red-700"
-              }`}
-              id="confirmPassword"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              placeholder="******************"
-              required
-            />
-            {confirmPasswordError && (
-              <p className="text-red-500 text-xs italic">
-                Password does not match.
-              </p>
-            )}
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                className={`shadow appearance-none font-light border rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline ${
+                  confirmPasswordError && "border-red-700"
+                }`}
+                id="password"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                placeholder="******************"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700  mb-2"
+                htmlFor="ConfirmPassword"
+              >
+                Confirm Password
+              </label>
+              <input
+                className={`shadow appearance-none font-light border rounded w-full py-2 px-3 text-gray-700  leading-tight focus:outline-none focus:shadow-outline ${
+                  confirmPasswordError && "border-red-700"
+                }`}
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                onChange={handleChange}
+                placeholder="******************"
+                required
+              />
+              {confirmPasswordError && (
+                <p className="text-red-500 text-xs italic">
+                  Password does not match.
+                </p>
+              )}
+            </div>
 
-          <div className="flex items-center justify-between">
-            <button
-              className="px-3 py-1 text-white bg-slate-500 hover:bg-slate-700 text-sm font-light"
-              type="submit"
-            >
-              Register
-            </button>
-          </div>
-        </form>
+            <div className="flex items-center justify-between">
+              <button className="px-3 py-1 text-white bg-slate-500 hover:bg-slate-700 text-sm font-light">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
